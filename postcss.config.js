4db104c3db6aaa422e5d526cacf6d6ffc80a644b
Andrei Mustata <hello@amoustache.net>
2022-05-17 19:37:12 +0200

    Gather things round

--- postcss.config.js
+++ postcss.config.js
@@ -2,29 +2,17 @@ module.exports = {
   plugins: {
     '@fullhuman/postcss-purgecss': {
       content: [
-        './layouts/**/*.html'
+        './hugo_stats.json'
       ],
-      whitelist: [
-        'hero','hero-body','has-text-right',
-        'highlight',
-        'pre',
-        'video',
-        'code',
-        'content',
-        'field',
-        'field-body',
-        'control',
-        'is-fullwidth',
-        'input',
-        'select',
-        'h3',
-        'h4',
-        'h5',
-        'h6',
-        'ul',
-        'li',
-        'is-accent-light'
-      ]
+      defaultExtractor: content => {
+        const els = JSON.parse(content).htmlElements;
+        return [
+          ...(els.tags || []),
+          ...(els.classes || []),
+          ...(els.ids || []),
+        ];
+      },
+      safelist: []
     }
   }
 };
